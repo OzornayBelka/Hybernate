a7675a3163d52bac3ecd3b1df2e5e6dcbbf73ced
JuliaGudkova <rrrr0012@icloud.com>
2024-02-24 18:59:10 +0300

    first

--- src/main/java/com/example/hibernate/entity/Users.java
+++ src/main/java/com/example/hibernate/entity/Users.java
@@ -3,18 +3,40 @@ package com.example.hibernate.entity;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Entity
 public class Users {
     @EmbeddedId
     private UsersKey usersKey;
-    private String phone_number;
-    private String city_of_living;
+    private String phoneNumber;
+    private String cityOfLiving;
 
+    @Override
+    public String toString() {
+        return "Users{" +
+                "usersKey=" + usersKey +
+                ", phone_number='" + phoneNumber + '\'' +
+                ", city_of_living='" + cityOfLiving + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return Objects.equals(usersKey, users.usersKey) && Objects.equals(phoneNumber, users.phoneNumber) && Objects.equals(cityOfLiving, users.cityOfLiving);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usersKey, phoneNumber, cityOfLiving);
+    }
 }
