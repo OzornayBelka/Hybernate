37eb3c763aab1178deb31f951620d943309b8bd4
JuliaGudkova <rrrr0012@icloud.com>
2024-02-24 18:59:10 +0300

    next

---
+++ src/main/java/com/example/hibernate/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.hibernate.controller;
+
+import com.example.hibernate.entity.Users;
+import com.example.hibernate.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private UserRepository repository;
+
+    @GetMapping("/persons/by-city")
+    public ResponseEntity<List<Users>> getByCity (@RequestParam String city) {
+        return ResponseEntity.ok(repository.findByCityOfLiving(city));
+    }
+    @GetMapping("/persons/by-age")
+    public ResponseEntity<List<Users>> getByAge(@RequestParam int age) {
+        return ResponseEntity.ok(repository.findByAgeLessThanOrderByAge(age));
+    }
+    @GetMapping("/persons/by-name-surname")
+    public ResponseEntity<Optional<Users>> getByNameAndSurname(@RequestParam String name, String surname){
+        return ResponseEntity.ok(repository.findByNameAndSurname(name, surname));
+    }
+
+
+}
--- src/main/java/com/example/hibernate/entity/UsersKey.java
+++ src/main/java/com/example/hibernate/entity/UsersKey.java
@@ -2,14 +2,38 @@ package com.example.hibernate.entity;
 
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
-@Data
+@Setter
+@Getter
 public class UsersKey implements Serializable {
     private String name;
     private String surname;
     private int age;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UsersKey usersKey)) return false;
+        return age == usersKey.age && Objects.equals(name, usersKey.name) && Objects.equals(surname, usersKey.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age);
+    }
+
+    @Override
+    public String toString() {
+        return "UsersKey{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
